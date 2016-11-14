6a35a24ef3d036a123fbece1c693482406e3eb22
michaelwishlow <michael@pantheritconsulting.com>
2016-11-14 12:27:42 -0800

    Add Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111211338) do
+ActiveRecord::Schema.define(version: 20161114183135) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161111211338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
